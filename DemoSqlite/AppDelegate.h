9ae663edcfefdbf2375a7b3037392acbb87473f9
menem <abdelmenim.mohamed97@gmail.com>
2018-02-26 14:46:22 +0200

    Initial Commit

---
+++ DemoSqlite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoSqlite
+//
+//  Created by mac on 2/26/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
